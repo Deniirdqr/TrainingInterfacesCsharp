bad8c2f144a8123cb71dcedec74f85f7560590ad
perkles <deniordqr@hotmail.com>
2022-04-21 20:02:25 -0300

    Add project files.

---
+++ TrainingThirteen/Services/RentalService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TrainingThirteen.Entities;
+
+namespace TrainingThirteen.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
